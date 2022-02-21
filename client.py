108c0e921f6029b8dda91351bb2cc8e778dbac65
shutdoor <enyssa2129@gmail.com>
2022-02-21 14:21:01 -0600

    Commenting on code & Removing Chat.py

--- client.py
+++ client.py
@@ -1,51 +1,57 @@
 import socketio
+import asyncio
 
+# Initialize Async Client Socket
 sio = socketio.AsyncClient()
 
+#Set default Socket Connect Event to inform client of their connecting
 @sio.event
 async def connect():
     print("You have connected.")
 
-@sio.event
-async def disconnect():
-    print("")
-
-@sio.event
-async def connect_error(e):
-    print(e)
-
+#Create Socket Event that will send message to clients terminal after
+#receiving it from the server
 @sio.event
 async def broadcast(message):
     print(message)
 
+#Create Background Task that will allow for all clients connected to the server
+#to message back and forth
 async def background():
+    #Boolean isActive allows for the application to know when to stop
     isActive = True
+    #Create while loop that will prompt for input between sleep cycles until prompted otherwise
     while isActive:
         message = input("Message: ")
         if(message == "/exit"):
             isActive = False
-            await sio.disconnect()
+            #Force the end of the script
+            exit()
         else:
+            #Emits types message to the server to then be broadcasted to all clients
             await sio.emit("receiver", message)
+            #Wait for any incoming messages that may have been sent during input
             await sio.sleep(1)
     
-async def runClient(HOST, PORT):
+#Main function of Client
+async def main():
+    #Prompt for Username to add diversity among connected clients
     username = input("Input Username: ")
     print("\n")
-    await sio.connect("http://"+ HOST +":"+ PORT,
+    #Connects client to server through Host - "Localhost" Port - "8000"
+    #Pass X-USERNAME to the server to allow for a session to carry that name instead of
+    #passing it through as a parm
+    await sio.connect("http://Localhost:8000",
                     headers={"X-USERNAME": username})
+    #Sleep to allow for all connections and messages to be displayed to current client
     await sio.sleep(3)               
    
-    # task = await sio.start_background_task(background) 
+    #Create background task that will prompt users for message that can then be sent to all
+    #other clients
+    task = await sio.start_background_task(background) 
     
-    isActive = True
-    while isActive:
-        message = input("Message: ")
-        if(message == "/exit"):
-            isActive = False
-            await sio.disconnect()
-        else:
-            await sio.emit("receiver", message)
-            await sio.sleep(1)
-    
+    #Wait until client has connected and then wait for disconnect
     await sio.wait()
+
+
+if __name__=="__main__": asyncio.run(main())
--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import socketio
 
+# Initialize Async Server Socket
 sio = socketio.AsyncServer(async_mode='asgi')
-app = socketio.ASGIApp(sio, static_files={
-    '': ''
-})
+# Initialize Async Server Socket ASGIApp
+app = socketio.ASGIApp(sio, static_files={'': ''})
 
+#Server Socket Default Connect Event
+#Pull username from the header X-USERNAME and then set the session["username"] to that
+#Use Sockets Session with clients sid to pull their session
+#Then broadcast that the username + " has connected" 
 @sio.event
 async def connect(sid, environ):
     username = environ.get("HTTP_X_USERNAME")
@@ -12,18 +16,29 @@ async def connect(sid, environ):
         session['username'] = username
     await sio.emit('broadcast', username + " has connected")
 
+#Server Socket Default Disconnect Event
+#Use Sockets Session with clients sid to pull their session
+#Get username from session["username"]
+#Then broadcast that the username + " has disconnected" 
 @sio.event
 async def disconnect(sid):
     async with sio.session(sid) as session:
+        #Emit to Client broadcast event
         await sio.emit('broadcast', session["username"] + " has disconnected")
 
+#Server Socket Event that will receive data.
+#Then broadcast that data to all connected clients
 @sio.event
 async def message(sid, data):
     print(sid," ",data)
+    #Emit data to Client broadcast event
     await sio.emit("broadcast", message)
 
+#Server Socket Event that will receive data add the client's username to the front of it.
+#Then broadcast it to all connected clients
 @sio.event
 async def receiver(sid, data):
     async with sio.session(sid) as session:
         message = session["username"] + " " +data
+    #Emit message string to Client broadcast event
     await sio.emit("broadcast", message)
